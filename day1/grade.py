437eca1fd3e9b2bf283112ce59fe8dc3ef36c33e
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-19 17:55:03 +0000

    added if else

---
+++ day1/grade.py
@@ -0,0 +1,8 @@
+marks =int(input("enter the your marks: "))
+if marks>=95:
+    print("you get A grade")
+elif marks>=80:
+    print("you get B grade")
+else:
+    print("you get failed")        
+    

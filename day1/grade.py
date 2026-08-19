fe9e28760a55eff3fcfb22378094f5908573723f
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-19 18:03:48 +0000

    added on elif

--- day1/grade.py
+++ day1/grade.py
@@ -4,7 +4,9 @@ if marks>=95:
 elif marks>=80:
     print("you get B grade")
 elif marks>=60:
-    print("you get C grade")    
+    print("you get C grade")  
+elif marks>=35:
+    print("you get D grade")       
 else:
     print("you get failed")        
     

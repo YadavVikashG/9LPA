ed3ea36255a22b11166432c121a6773e10969bec
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-19 19:08:43 +0000

    added loop

---
+++ day1/loop.py
@@ -0,0 +1,14 @@
+for i in range(0,6):
+    print(i)
+    #its output will be 0,1,2,3,4,5
+for a in range(5):
+    print(a)
+    #its output will be 0,1,2,3,4
+#above is for loop and below in while loop
+
+count=1
+while count<=5:
+    print(count)
+    count+=1
+    #its output will be 1,2,3,4,5
+

2f9f8ca0b08f82a081de5d41dde47889ac187cd0
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-20 19:45:31 +0000

    problem solution file

---
+++ day1/problem.py
@@ -0,0 +1,19 @@
+#Problem 1
+age=int(input("enter your age:"))
+if age<13:
+    print("you are teenager")
+elif age>=20:
+    print("you are adult ")
+#Problem 2
+num=int(input("enter your number to know is odd or evern:"))
+if num%2==0:
+    print("your number evern")
+else:
+    print("your number odd ")
+#Problem 3  for eveen== for odd !=
+for i in range(1,101):
+    if i % 2 != 0 :
+        print(i)
+
+
+ 

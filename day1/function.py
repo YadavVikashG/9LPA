e73d6b4776880e21bee84776ea3d4116f7556dbf
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-20 19:25:33 +0000

    the functin file is added

---
+++ day1/function.py
@@ -0,0 +1,13 @@
+a=10
+b=10
+c=a+b
+print(c)
+#we have to write above if if want to add two number istend of this we can create an function and call it every time.
+def add(a,b):
+    return a+b
+result=add(20,49)
+print(result)
+def ever_is(number):
+    return number%2==0
+print( ever_is(c))
+print( ever_is(result))

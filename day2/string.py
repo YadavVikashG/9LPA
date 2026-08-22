91aba4fdd2ee5ff85a6603c8f5fb8789afac5204
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-22 14:31:05 +0000

    day 2 string study complited

--- day2/string.py
+++ day2/string.py
@@ -1,7 +1,20 @@
 name="vikash"
+print(name)
 print(name[-5:-1])
 #revers string
 print(name[1::3])#only tack letter 1 and 4
 print(name[::-1])
 print(name[1:4])#only tack letter 1,2,3
+text="this is vikash"
+print(text.upper())#THIS IS VIKASH
+print(text.lower())#this is vikash
+print(text.capitalize())#This is vikash
+print(text.replace("vikash","jaisey"))#only replace not change the actual word
+word=text.split()
+print(word)
+languages = ["Python", "Java", "C++"]
+wordjoin="-".join(languages)
+print(wordjoin)
+name="J"+name[1:]
+print(name)
 

daddafc72b423896ce4cb7663874421bee1e8e5a
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-23 13:45:01 +0000

    funtion  file added

--- day2/function.py
+++ day2/function.py
@@ -12,3 +12,15 @@ def add():
     print(userName)
 add()    
 # print(userName)    
+
+def add1(a,b):
+    
+    return a+b
+add1(10,20)     #it will display the sum of 10 and 20 but it will not return the value to the main program so we can store it in a variable and print it
+result=add1(30,20) #it will return the value to the main program and we can store it in a variable and print it
+print(result)
+
+
+
+
+

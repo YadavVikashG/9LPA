684bb569d20a3e7d51aabdcfec7134fdff5f49b3
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-23 14:10:02 +0000

    day2 project file added

---
+++ day2/project.py
@@ -0,0 +1,12 @@
+password=input("Enter your password: ")
+if len(password)<8:
+    print("Password must be at least 8 characters long.")
+elif not any(char.isdigit() for char in password):
+    print("Password must contain at least one digit.")
+elif not any(char.isupper() for char in password):
+    print("Password must contain at least one uppercase letter.")
+elif not any(char.islower() for char in password): 
+    print("Password must contain at least one lowercase letter.")
+else:
+    print("Password is valid.")
+        

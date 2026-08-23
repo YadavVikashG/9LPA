6bdbb06e6dc42e6e3ec1f12754aecbeeb13a7646
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-23 13:20:24 +0000

    dictionary  file added

---
+++ day2/dictionary.py
@@ -0,0 +1,10 @@
+student={
+    "name":"radha",
+    "gender":"male",
+    "age":24
+}
+print(student.get("name"))
+print(student.keys())
+print(student.values())
+for key, value in student.items():
+    print(key,value)

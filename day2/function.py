51980d7456e98ce1f1ecdf9af1d383f6bfaba2df
YADAV VIKASH GAJRAJ <yadavvikashgajraj@gmail.com>
2026-08-23 13:39:19 +0000

    funtion  file added

--- day2/function.py
+++ day2/function.py
@@ -3,3 +3,12 @@ def user(name):
 
 userName=input("please enter your name :")
 print(user(userName))
+
+#above is given an scope of the variable above userName is global varible but 
+#below is local variable for funtion
+
+def add():
+    userName="radha"#Prefer local variables and avoid unnecessary global state.
+    print(userName)
+add()    
+# print(userName)    
